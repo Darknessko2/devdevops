7f7c5498f85d880551fa4f86935b18733b39f8e7
GLH <135549622+Darknessko2@users.noreply.github.com>
2024-05-31 21:00:53 +0200

    fin

--- hola.py
+++ hola.py
@@ -1,1 +1,13 @@
-print('holaaaasasdfdfsdfa')
+from datetime import datetime
+try:
+    fecha_input = input("Ingresa la fecha con el siguiente formato dia/mes/anyo: ")
+    formato = "%d/%m/%Y"
+
+    fecha = datetime.strptime(fecha_input,formato)
+    print(f'el dia es {fecha.day}, el mes es {fecha.month} y el anio es {fecha.year}')
+except:
+    print('Fecha no valida')
+
+
+
+    
