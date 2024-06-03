6c40f51eb5329eeb5b09d7ed8e176c194a7c8037
GLH <135549622+Darknessko2@users.noreply.github.com>
2024-06-03 16:10:03 +0200

    commit en nueva rama

--- hola.py
+++ hola.py
@@ -4,6 +4,7 @@ try:
     formato = "%d/%m/%Y"
 
     fecha = datetime.strptime(fecha_input,formato)
+    
     print(f'el dia es {fecha.day}, el mes es {fecha.month} y el anio es {fecha.year}')
 except:
     print('Fecha no valida')
